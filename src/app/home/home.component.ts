05c56484f751d2f98d775c4192776d0206d3431e
Florian Tischler <Florian.Tischler@gmx.net>
2024-06-05 17:43:05 +0200

    implement counter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-import { MyCardComponent, MyCardHeaderDirective } from '../my-card/my-card.component';
+import {
+  MyCardBodyDirective,
+  MyCardComponent,
+  MyCardFooterDirective,
+  MyCardHeaderDirective,
+} from '../my-card/my-card.component';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [MyCardComponent, MyCardHeaderDirective],
+  imports: [MyCardComponent, MyCardHeaderDirective, MyCardBodyDirective, MatButton, MyCardFooterDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export default class HomeComponent {}
+export default class HomeComponent {
+  counter = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    if (this.counter > 0) this.counter--;
+  }
+}
